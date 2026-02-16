b9fb3681dd9b51a0484276b162965dd46b9f3b01
Mateo <mateogs897@gmail.com>
2026-02-16 09:54:11 -0500

    Added create_vault function

--- core-cpp/src/vault_manager.cpp
+++ core-cpp/src/vault_manager.cpp
@@ -6,3 +6,32 @@ VaultManager::VaultManager()
     : unlocked_(false)
 {    
 }
+
+bool VaultManager::create_vault(const std::string& master_password){
+    if (master_password.empty()){
+        return false;
+    }
+
+    salt_.resize(crypto_pwhash_SALTBYTES);
+    randombytes_buf(salt_.data(), salt_.size());
+
+    if (!derive_key(master_password, salt_, current_key_)){
+        return false;
+    }
+
+    std::string verification = "VAULT_OK"
+;
+
+if (!encrypt_string(
+    verification, 
+    current_key_,
+    verification_nonce_,
+    verification_ciphertext_)){
+        return false;
+    }
+
+    unlocked_ = true;
+
+    return true;
+}
+
